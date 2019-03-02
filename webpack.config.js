eae5e05757b0c2a427002159fd04d20d316fb9e2
Ari <jaljung@hanmail.net>
2019-03-02 17:05:42 -0500

    new update repository

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/src/index.js',
+    output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'build.js'
+  },
+
+  devServer:{
+    port: 3000,
+    contentBase: path.resolve(__dirname, 'dist'),
+  },
+
+  module: {
+    rules: [
+    {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: ['babel-loader']
+      }]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './client/public/index.html',
+      filename: './index.html'
+    })
+  ]
+}
