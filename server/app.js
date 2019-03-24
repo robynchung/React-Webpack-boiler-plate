137d04b7aba5fd8fd5e8d38c76b614f4ec3be7ee
Robyn Chung <jaljung@hanmail.net>
2019-03-24 11:18:56 -0400

    controller and routes added

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const router = require('./routes');
 
 const app = express();
 
 mongoose.connect('url');
 app.use(bodyParser.json());
+app.use(router);
 
 app.listen(4000);
