a0015b4be38b96e1eafaf833adfab7b2aa101787
Aeri <rjungdesign@gmail.com>
2020-10-07 20:29:36 -0400

    update

--- frontend/src/Index.js
+++ frontend/src/Index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../styles/index.scss';
 
-const App = () => (
-  <div>
-    <h1>Hello world!!</h1>
-    This is boiler plate of React with webpack
-  </div>
-);
+function App() {
+  return (
+    <div>
+      <h1>Hello world!!</h1>
+      This is boiler plate of React with webpack
+    </div>
+  );
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
